fc89e962688aa31cf25a9b27360db9da2054196b
loicfarel <loicfarele@gmail.com>
2024-10-15 13:34:46 +0100

    tools factuel & perception

--- src/views/dashboard/ToolsPerception.vue
+++ src/views/dashboard/ToolsPerception.vue
@@ -8,8 +8,11 @@ import VButton from "@/components/news/VButton.vue";
 import OngService from "../../services/modules/ong.service";
 import AuthService from "@/services/modules/auth.service";
 import InputForm from "@/components/news/InputForm.vue";
+import { useRouter } from "vue-router";
 
 const TYPE_ORGANISATION = "organisation";
+
+const router = useRouter();
 
 const payload = reactive({
   organisationId: "",
@@ -17,7 +20,7 @@ const payload = reactive({
 });
 const formData = reactive({});
 const organisations = ref([]);
-const formDataFactuel = ref([]);
+const formDataPerception = ref([]);
 const isOrganisation = ref(false);
 const isLoading = ref(false);
 const isLoadingDataFactuel = ref(true);
@@ -26,9 +29,9 @@ const idEnquete = "EaPR3GQnP1z2YvMVZXEL0QorKA7BmkNLzWlnw9egqGOjbxJd3Ra68p4Dql46Y
 const idProgramme = "Kd6Zov9ybW9PRxngKpQv81oeXMr6YOJgmV5ZlG47dkq2zjwABDLma3y0DGj3BP7w";
 
 const getDataFormFactuel = async () => {
-  await FormulaireFactuel.getDataFormFactuel(idProgramme)
+  await FormulaireFactuel.getDataFormPerception(idProgramme)
     .then((result) => {
-      formDataFactuel.value = result.data.data;
+      formDataPerception.value = result.data.data;
       isLoadingDataFactuel.value = false;
       initializeFormData();
       // const savedData = localStorage.getItem("formData");
@@ -41,7 +44,7 @@ const getDataFormFactuel = async () => {
     .catch((e) => {
       console.error(e);
       isLoadingDataFactuel.value = false;
-      toast.error("Une erreur est survenue: Liste du formulaire Factuel .");
+      toast.error("Une erreur est survenue: Liste du formulaire Perception .");
     });
 };
 const getOrganizations = async () => {
@@ -72,10 +75,10 @@ const submitData = async () => {
   console.log("formDat:", formData);
   isLoading.value = true;
   const response = {
-    factuel: Object.keys(formData).map((indicateurId) => ({
+    perception: Object.keys(formData).map((indicateurId) => ({
       indicateurDeGouvernanceId: indicateurId,
       optionDeReponseId: formData[indicateurId].selectedOption,
-      source: formData[indicateurId].source,
+      commentaire: formData[indicateurId].source,
     })),
   };
 
@@ -91,22 +94,18 @@ const submitData = async () => {
       console.error(e);
       isLoading.value = false;
       payload.response_data = [];
-      toast.error(`${result.data.message}`);
+      toast.error("Erreur pour la collecte des données");
     });
 };
 const initializeFormData = () => {
-  formDataFactuel.value.flatMap((type_gouvernance) => {
-    type_gouvernance.principes_de_gouvernance.flatMap((principe) =>
-      principe.criteres_de_gouvernance.flatMap((critere) =>
-        critere.indicateurs_de_gouvernance.map((indicateur) => {
-          formData[indicateur.id] = {
-            selectedOption: null,
-            source: "source info",
-          };
-        })
-      )
-    );
-  });
+  formDataPerception.value.flatMap((critere) =>
+    critere.indicateurs_de_gouvernance.map((indicateur) => {
+      formData[indicateur.id] = {
+        selectedOption: null,
+        commentaire: "source info",
+      };
+    })
+  );
 };
 const changePage = (pageNumber) => {
   currentPage.value = pageNumber;
@@ -129,9 +128,9 @@ onMounted(async () => {
 });
 </script>
 <template>
-  <h2 class="mt-10 text-lg font-medium intro-y">Formulaire Factuel</h2>
+  <h2 class="mt-10 text-lg font-medium intro-y">Outils de perception</h2>
   <div class="w-full p-4 font-bold text-center text-white uppercase rounded bg-primary">programme 1</div>
-  <div v-if="organisations.length > 0 && isOrganisation" class="flex justify-end mt-5">
+  <div v-if="organisations.length > 0 && isOrganisation" class="flex justify-end my-5">
     <div class="">
       <label class="form-label">Organisations</label>
       <TomSelect v-model="payload.organisationId" :options="{ placeholder: 'Selectionez un programme' }" class="w-full">
@@ -142,60 +141,45 @@ onMounted(async () => {
   <div>
     <div v-if="!isLoadingDataFactuel" class="py-5 intro-x">
       <div class="space-y-8">
-        <!-- vfor type_gouvernace -->
-        <div v-show="typeGouvernanceIndex === currentPage" v-for="(typeGouvernance, typeGouvernanceIndex) in formDataFactuel" :key="typeGouvernanceIndex" class="transition-all">
-          <h1 class="mb-5 text-2xl font-semibold text-gray-800">{{ typeGouvernance.nom }}</h1>
+        <div class="space-y-6">
           <!-- vfor Principe -->
-          <div class="space-y-6">
-            <AccordionGroup :selectedIndex="null" v-for="(principe, principeIndex) in typeGouvernance.principes_de_gouvernance" :key="principeIndex" class="border-primary">
-              <AccordionItem class="">
-                <Accordion class="text-xl !px-4 font-semibold bg-primary !text-white flex items-center justify-between">
-                  <h2>{{ principe.nom }}</h2>
-                  <ChevronDownIcon />
-                </Accordion>
-                <AccordionPanel class="!px-8 !shadow-md !bg-white !py-6">
-                  <!-- vfor Critere -->
-                  <AccordionGroup class="space-y-2">
-                    <AccordionItem class="!px-0" v-for="(critere, critereIndex) in principe.criteres_de_gouvernance" :key="critereIndex">
-                      <Accordion class="text-xl !p-4 font-semibold bg-primary/90 !text-white flex items-center justify-between">
-                        <h2>{{ critere.nom }}</h2>
-                        <ChevronDownIcon />
-                      </Accordion>
-                      <!-- vfor Indicateur -->
-                      <AccordionPanel class="!border-none pt-1">
-                        <div v-for="(indicateur, indicateurIndex) in critere.indicateurs_de_gouvernance" :key="indicateurIndex" class="relative px-4 pt-2 my-3 transition-all">
-                          <div class="p-2 py-3 space-y-2 border-l-8 border-yellow-500 rounded shadow box">
-                            <p class="w-full text-lg font-semibold text-center text-primary">{{ indicateurIndex + 1 }} - {{ indicateur.nom }}</p>
-                            <div class="flex flex-col items-center justify-center w-full gap-3">
-                              <!-- vfor Option -->
-                              <div class="inline-flex flex-wrap items-center gap-3">
-                                <div v-if="formData[indicateur.id]" v-for="(option, optionIndex) in indicateur.options_de_reponse" :key="optionIndex">
-                                  <input v-model="formData[indicateur.id].selectedOption" :id="`radio-${indicateur.id}${optionIndex}`" class="form-check-input" type="radio" :name="'option-' + indicateur.id" :value="option.id" />
-                                  <label class="text-base form-check-label" :for="`radio-${indicateur.id}${optionIndex}`">
-                                    {{ option.libelle }}
-                                  </label>
-                                </div>
-                              </div>
-                              <div v-if="formData[indicateur.id]">
-                                <label :for="`${indicateur.id}`" class="form-label">Source</label>
-                                <input :id="`${indicateur.id}`" type="text" required v-model="formData[indicateur.id].source" class="form-control" placeholder="Source" />
-                              </div>
-                            </div>
-                          </div>
+          <AccordionGroup class="space-y-2">
+            <AccordionItem class="!px-0" v-for="(principe, principeIndex) in formDataPerception" :key="principeIndex">
+              <Accordion class="text-xl !p-4 font-semibold bg-primary/90 !text-white flex items-center justify-between">
+                <h2>{{ principe.nom }}</h2>
+                <ChevronDownIcon />
+              </Accordion>
+              <!-- vfor Indicateur -->
+              <AccordionPanel class="!border-none pt-1">
+                <div v-for="(indicateur, indicateurIndex) in principe.indicateurs_de_gouvernance" :key="indicateurIndex" class="relative px-4 pt-2 my-3 transition-all">
+                  <div class="p-2 py-3 space-y-2 border-l-8 border-yellow-500 rounded shadow box">
+                    <p class="w-full text-lg font-semibold text-center text-primary">{{ indicateurIndex + 1 }} - {{ indicateur.nom }}</p>
+                    <div class="flex flex-col items-center justify-center w-full gap-3">
+                      <!-- vfor Option -->
+                      <div class="inline-flex flex-wrap items-center gap-3">
+                        <div v-if="formData[indicateur.id]" v-for="(option, optionIndex) in indicateur.options_de_reponse" :key="optionIndex">
+                          <input v-model="formData[indicateur.id].selectedOption" :id="`radio-${indicateur.id}${optionIndex}`" class="form-check-input" type="radio" :name="'option-' + indicateur.id" :value="option.id" />
+                          <label class="text-base form-check-label" :for="`radio-${indicateur.id}${optionIndex}`">
+                            {{ option.libelle }}
+                          </label>
                         </div>
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </AccordionGroup>
-                </AccordionPanel>
-              </AccordionItem>
-            </AccordionGroup>
-          </div>
+                      </div>
+                      <div v-if="formData[indicateur.id]">
+                        <label :for="`${indicateur.id}`" class="form-label">Commentaire</label>
+                        <input :id="`${indicateur.id}`" type="text" required v-model="formData[indicateur.id].commentaire" class="form-control" placeholder="Commentaire" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </AccordionPanel>
+            </AccordionItem>
+          </AccordionGroup>
         </div>
       </div>
     </div>
     <LoaderSnipper v-else />
     <div class="flex justify-center gap-3 my-8">
-      <button v-for="(item, index) in formDataFactuel" @click="changePage(index)" :class="index === currentPage ? 'btn-primary' : 'btn-outline-primary'" class="px-4 py-3 btn" :key="index">{{ index + 1 }}</button>
+      <button v-for="(item, index) in formDataPerception" @click="changePage(index)" :class="index === currentPage ? 'btn-primary' : 'btn-outline-primary'" class="px-4 py-3 btn" :key="index">{{ index + 1 }}</button>
     </div>
     <div class="flex justify-center w-full">
       <VButton label="Soumettre" class="px-8 py-3 w-max" :loading="isLoading" @click="submitData" />
